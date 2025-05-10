adc693e488df9f4c227baafca2e1130eead3987d
Alexander Nittmann <a00542381@unet.univie.ac.at>
2025-05-10 14:27:36 +0200

    update datasets; add analyze graph file

--- tests/test_both.py
+++ tests/test_both.py
@@ -9,18 +9,29 @@ from thesis.utils.test_utils import evaluate_quasistable_cv, evaluate_wl_cv, eva
     get_stats_from_test_results_csv
 
 dataset_names = [
-    "PTC_FM"
+    # # small datasets
     # "KKI",
-    # "ENZYMES",
+    # "PTC_FM",
     # "MSRC_9",
-    # "IMDB-BINARY",
-    # "REDDIT-BINARY",
-    # "EGO-1",
-    # "EGO-2"
-    # "EGO-3",
-    # "NCI1",
+
+    # # large datasets
+    # "COLLAB",
     # "DD",
-    # "EGO-4"
+    # "REDDIT-BINARY",
+
+    # # medium datsets
+    # "IMDB-BINARY",
+    # "NCI1",
+
+    # # social network datasets
+    # "EGO-1",
+    # "EGO-2",
+    # "EGO-3",
+    # "EGO-4",
+
+    # # new datasets
+    # "ENZYMES",
+    # "PROTEINS"
 ]
 
 for dataset_name in dataset_names:
@@ -39,7 +50,7 @@ for dataset_name in dataset_names:
     graph_id_label_map = {g.graph["graph_id"]: g.graph["graph_label"] for g in graphs}
 
     # h_grid = list(range(1, 11))
-    h_grid = [8]
+    h_grid = [39]
     k_grid = [2, 4, 8, 16]
     q_grid = [2**i for i in range(3, -1, -1)] + [0]
     n_max = 1024
@@ -48,16 +59,16 @@ for dataset_name in dataset_names:
 
     c_grid = [10**i for i in range(-3, 4)]  # SVM C ∈ {1e-3 to 1e3}
 
-    evaluate_quasistable_cv(
-        disjoint_graph, graph_id_label_map, refinement_steps_grid=refinement_steps_grid, c_grid=c_grid, folds=10, dataset_name=dataset_name, repeats=1, start_repeat=1
-    )
+    # evaluate_quasistable_cv(
+    #     disjoint_graph, graph_id_label_map, refinement_steps_grid=refinement_steps_grid, c_grid=c_grid, folds=10, dataset_name=dataset_name, repeats=1, start_repeat=1
+    # )
 
     # res_dir = "../tests/MSRC_9-Evaluation-QSC-20250506_111034"
 
     # get_stats_from_test_results_csv(os.path.join(res_dir, "test_results.csv"))
 
-    # evaluate_wl_cv(
-    #     disjoint_graph, graph_id_label_map, h_grid, c_grid, folds=10, dataset_name=dataset_name, repeats=1, start_repeat=1
-    # )
+    evaluate_wl_cv(
+        disjoint_graph, graph_id_label_map, h_grid, c_grid, folds=10, dataset_name=dataset_name, repeats=1, start_repeat=1
+    )
     #
     # evaluate_gwl_cv(disjoint_graph, graph_id_label_map, h_grid, k_grid, c_grid, dataset_name=dataset_name, folds=10, repeats=5, start_repeat=1)
--- thesis/test/analyze_graph.py
+++ thesis/test/analyze_graph.py
@@ -1,7 +1,4 @@
-import networkx as nx
-import numpy as np
-
-from thesis.evaluation.qsc_evaluation import QscEvaluation
+from thesis.utils.other_utils import analyze
 from thesis.utils.read_data_utils import dataset_to_graphs
 
 dataset_names = [
@@ -31,12 +28,5 @@ dataset_names = [
 ]
 
 for dataset_name in dataset_names:
-
     graphs = dataset_to_graphs("../../data", dataset_name)
-    disjoint_graph = nx.disjoint_union_all(graphs)
-    graph_id_label_map = {g.graph["graph_id"]: g.graph["graph_label"] for g in graphs}
-
-    max_step = np.inf
-
-    qsc_evaluation = QscEvaluation(dataset_name, disjoint_graph, graph_id_label_map)
-    qsc_evaluation.refine_and_create_feature_vector_matrices(q=0)
+    analyze(graphs)
